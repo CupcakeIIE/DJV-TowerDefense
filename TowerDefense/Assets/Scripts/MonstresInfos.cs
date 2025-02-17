48faed9abd88b14a7d58a2ff7f3edd1143a9864b
CupcakeIIE <122415151+CupcakeIIE@users.noreply.github.com>
2025-02-17 15:20:42 +0100

    Scriptable Object Monstres

---
+++ TowerDefense/Assets/Scripts/MonstresInfos.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class MonstresInfos : ScriptableObject
+{
+    public int speed;
+    public int health;
+    public int score;
+    public int gold;
+
+    // public GameObject monstre;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
