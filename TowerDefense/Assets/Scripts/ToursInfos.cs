e10655119598ee996f7410b6efd086c424298f68
CupcakeIIE <122415151+CupcakeIIE@users.noreply.github.com>
2025-02-17 16:07:54 +0100

    Scriptable Object Tours

---
+++ TowerDefense/Assets/Scripts/ToursInfos.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ToursInfos : ScriptableObject
+{
+    public int prix;
+    public int projectiles;
+    public float frequence;
+    public float vitesse;
+    public int degats;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
