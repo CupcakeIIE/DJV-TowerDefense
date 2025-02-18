cdeca709b5d5c613a08bcceac965424717db3589
CupcakeIIE <122415151+CupcakeIIE@users.noreply.github.com>
2025-02-18 12:48:49 +0100

    Spawn rate différents

--- TowerDefense/Assets/Scripts/Spawner.cs
+++ TowerDefense/Assets/Scripts/Spawner.cs
@@ -47,8 +47,30 @@ public class Spawner : MonoBehaviour
         {
             if (loadedMonstres.Count > 0)
             {
+                GameObject monstrePrefab;
+
+                float randomValue = Random.Range(0f, 1f);
+                //Debug.Log(randomValue);
+                if (randomValue < 0.55f)
+                {
+                    // gobelin
+                    // Debug.Log("gobelin");
+                    monstrePrefab = loadedMonstres[1];
+                }
+                else if (randomValue < 0.85f)
+                {
+                    // troll
+                    // Debug.Log("troll");
+                    monstrePrefab = loadedMonstres[2];
+                }
+                else
+                {
+                    // dragon
+                    // Debug.Log("dragon");
+                    monstrePrefab = loadedMonstres[0];
+                }
                 // Select a random enemy from the loaded list
-                GameObject monstrePrefab = loadedMonstres[Random.Range(0, loadedMonstres.Count)];
+                // GameObject monstrePrefab = loadedMonstres[Random.Range(0, loadedMonstres.Count)];
 
                 // Instantiate the enemy at the spawner's position and rotation
                 Instantiate(monstrePrefab, this.transform.position, this.transform.rotation);
