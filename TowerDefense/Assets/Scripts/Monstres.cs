6c48e5c377a3905e34f52b09aa421a2b9f458857
CupcakeIIE <122415151+CupcakeIIE@users.noreply.github.com>
2025-02-18 17:16:55 +0100

    Deplacement monstres

---
+++ TowerDefense/Assets/Scripts/Monstres.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monstres : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        if (waypointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);      
+            return;  
+        }
+
+        waypointIndex++;
+        target = Waypoints.points[waypointIndex];
+    }
+}
